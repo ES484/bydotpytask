7cf100ba9ec23b404e800180592dfb003f2f488d
ES484 <esraasaad478@gmail.com>
2023-10-03 23:32:25 +0200

    sign up ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary_BG: '#7244C8',
+        primaryTextColor: '#475467'
+      }
     },
   },
   plugins: [],
